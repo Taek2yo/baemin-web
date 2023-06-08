506723ad9b8a1040acf5407a34a4a843da7e0241
Mintaek2 <789rkd@naver.com>
2023-06-08 17:12:24 +0900

    fix : 컴포넌트 영역 벗어나서 드래그 이벤트작동 하는것 수정

--- src/hooks/useDraggable.js
+++ src/hooks/useDraggable.js
@@ -1,20 +1,30 @@
 import { useState, useEffect, useRef } from 'react';
 
-export default function useDraggable(){
+export default function useDraggable() {
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const containerRef = useRef(null);
+  const isOutsideContainerRef = useRef(false); // 새로운 useRef 추가
 
   useEffect(() => {
     const handleMouseUp = () => {
       setIsDragging(false);
+      isOutsideContainerRef.current = false; // 드래그 종료 시 컨테이너 영역 밖 상태 초기화
+    };
+
+    const handleMouseLeave = () => {
+      if (isDragging) {
+        isOutsideContainerRef.current = true; // 컨테이너 영역을 벗어났을 때 상태 변경
+      }
     };
 
     const containerElement = containerRef.current;
     containerElement.addEventListener("mouseup", handleMouseUp);
+    containerElement.addEventListener("mouseleave", handleMouseLeave); // mouseleave 이벤트 추가
 
     return () => {
       containerElement.removeEventListener("mouseup", handleMouseUp);
+      containerElement.removeEventListener("mouseleave", handleMouseLeave); // mouseleave 이벤트 제거
     };
   }, []);
 
@@ -30,7 +40,11 @@ export default function useDraggable(){
     const deltaX = startX - x;
     containerRef.current.scrollLeft += deltaX;
     setStartX(x);
+
+    if (isOutsideContainerRef.current) {
+      setIsDragging(false); // 컨테이너 밖으로 나갔을 때 드래그 중지
+    }
   };
 
   return { containerRef, handleMouseDown, handleMouseMove };
-};
+}
