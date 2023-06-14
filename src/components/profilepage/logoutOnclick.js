cc1646f732f894b8a327aa1bc88c1d0468f270f5
Mintaek2 <789rkd@naver.com>
2023-06-14 20:06:15 +0900

    feat : 로그아웃 성공시 /mypage

--- src/components/profilepage/logoutOnclick.js
+++ src/components/profilepage/logoutOnclick.js
@@ -1,8 +1,20 @@
 'use client'
-import { signOut } from 'next-auth/react'
-export default function LogoutOnclick(){
-    return(
-        <span className="out" onClick={()=>{ signOut() }}>로그아웃</span>
-    )
-}
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
+export default function LogoutOnclick() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await signOut({
+      callbackUrl: '/mypage',
+    });
+    router.push('/mypage');
+  };
+
+  return (
+    <span className="out" onClick={handleLogout}>
+      로그아웃
+    </span>
+  );
+}
