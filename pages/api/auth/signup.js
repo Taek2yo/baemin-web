36062f0e5783752567e67670051107e87f2bb0dc
Mintaek2 <789rkd@naver.com>
2023-06-14 19:55:31 +0900

    fix : 회원가입 후 로그인 페이지로

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -7,6 +7,6 @@ export default async function signUphandler(req, res) {
       req.body.password = hash;
       let db = (await connectDB).db('baemin');
       await db.collection('accounts').insertOne(req.body);
-      res.status(200).json('성공');
+      res.redirect(302, '/login');
   }
 }; 
