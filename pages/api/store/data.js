28c7544b532efa84eba954e3a3743e69d39add65
Mintaek2 <789rkd@naver.com>
2023-06-24 15:45:35 +0900

    feat : 가게 정보 api 오류 처리 추가

--- pages/api/store/data.js
+++ pages/api/store/data.js
@@ -1,7 +1,17 @@
 import { connectDB } from "@/util/database";
 
-export default async function storeDataHandler(req,res){
+export default async function storeDataHandler(req, res) {
+  try {
+    if (req.method !== 'GET') {
+      res.status(400).json({ message: 'Bad Request' });
+      return;
+    }
+
     const db = (await connectDB).db("baemin");
-    let result = await db.collection('store').find().toArray()
-    res.status(200).json(result)
+    const result = await db.collection('store').find().toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('데이터 가져오기 에러:', error);
+    res.status(500).json({ message: '서버 오류' });
+  }
 }
