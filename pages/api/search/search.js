1ced606e2dd4793f28c04248554f4fa3d95bab33
Mintaek2 <789rkd@naver.com>
2023-07-11 21:49:09 +0900

    fix : 검색기능 수정

--- pages/api/search/search.js
+++ pages/api/search/search.js
@@ -3,24 +3,31 @@ import { connectDB } from "@/util/database";
 export default async function SearchHandler(req, res) {
   try {
     if (req.method !== "GET") {
-      res.status(400).json({ message: "Bad Request" });
+      res.status(400).json({ message: "잘못된 요청입니다." });
       return;
     }
+
     const searchTerm = req.query.searchTerm;
     const db = (await connectDB).db("baemin");
-    const result = await db
-      .collection("menu")
+
+    const storeResult = await db
+      .collection("store")
       .find({
-        $or: [
-          { menu_info: { $elemMatch: { desc: { $regex: new RegExp(`${searchTerm}`, "i") } } } },
-          { store_name: { $regex: new RegExp(`${searchTerm}`, "i") } },
-        ],
+        title: { $regex: new RegExp(`${searchTerm}`, "i") },
       })
       .toArray();
-    res.status(200).json(result);
 
+    const storeNames = storeResult.map((item) => item.title);
+    const menuResult = await db
+      .collection("menu")
+      .find({ $or: [
+        { menu_info: { $elemMatch: { desc: { $regex: new RegExp(`${searchTerm}`, "i") } } } },
+        { store_name: { $in: storeNames } },
+      ], })
+      .toArray();
+    res.status(200).json({ storeResult, menuResult });
   } catch (error) {
-    console.error("데이터 가져오기 에러:", error);
+    console.error("데이터 가져오기 오류:", error);
     res.status(500).json({ message: "서버 오류" });
   }
 }
