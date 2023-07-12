21ba09abded45b39c126b73f86b461a435bba040
Mintaek2 <789rkd@naver.com>
2023-07-12 16:10:13 +0900

    feat : store 컬렉션 menu 컬렉션 join

--- pages/api/search/search.js
+++ pages/api/search/search.js
@@ -1,31 +1,45 @@
 import { connectDB } from "@/util/database";
 
-export default async function SearchHandler(req, res) {
+export default async function searchTest(req, res) {
+  const searchTerm = req.query.searchTerm;
+  const db = (await connectDB).db("baemin");
+  
   try {
     if (req.method !== "GET") {
       res.status(400).json({ message: "잘못된 요청입니다." });
       return;
     }
-
-    const searchTerm = req.query.searchTerm;
-    const db = (await connectDB).db("baemin");
-
-    const storeResult = await db
-      .collection("store")
-      .find({
-        title: { $regex: new RegExp(`${searchTerm}`, "i") },
-      })
-      .toArray();
-
-    const storeNames = storeResult.map((item) => item.title);
-    const menuResult = await db
-      .collection("menu")
-      .find({ $or: [
-        { menu_info: { $elemMatch: { desc: { $regex: new RegExp(`${searchTerm}`, "i") } } } },
-        { store_name: { $in: storeNames } },
-      ], })
-      .toArray();
-    res.status(200).json({ storeResult, menuResult });
+    
+    const result = await db.collection("menu").aggregate([
+      {
+        $lookup: {
+          from: "store",
+          let: { menuIdString: { $toString: "$_id" } },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$$menuIdString", "$menuId"]
+                }
+              }
+            }
+          ],
+          as: "store_info",
+        },
+      },
+      {
+        $match: {
+          $or: [
+            {
+              "menu_info.name": { $regex: new RegExp(`${searchTerm}`, "i") },
+            },
+            { store_name: { $regex: new RegExp(`${searchTerm}`, "i") } },
+          ],
+        },
+      },
+    ]).toArray();
+    
+    res.status(200).json(result);
   } catch (error) {
     console.error("데이터 가져오기 오류:", error);
     res.status(500).json({ message: "서버 오류" });
