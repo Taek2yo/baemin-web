d0cf83518131082b2a97c465d46a23708166b417
Mintaek2 <789rkd@naver.com>
2023-07-25 17:56:02 +0900

    feat : 회원가입시 address 필드 추가

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -6,6 +6,7 @@ export default async function signUphandler(req, res) {
       const hash = await bcrypt.hash(req.body.password, 10);
       req.body.password = hash;
       req.body.profileImage = "";
+      req.body.address = [];
       let db = (await connectDB).db('baemin');
       await db.collection('accounts').insertOne(req.body);
       res.redirect(302, '/login');
