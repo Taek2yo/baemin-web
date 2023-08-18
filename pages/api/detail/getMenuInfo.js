f345f97b3c0baa9424a42651c947e93ec124dd6c
Mintaek2 <789rkd@naver.com>
2023-08-18 17:13:02 +0900

    feat : 장바구니 기능을 위한 데이터 추가 불러오기

--- pages/api/detail/getMenuInfo.js
+++ pages/api/detail/getMenuInfo.js
@@ -7,16 +7,25 @@ export default async function getMenuInfoHandler(req, res) {
   const menuId = query.menuId;
   try {
     const db = (await connectDB).db('baemin');
-    // 상점 정보 조회 후 일치하는 메뉴 정보 가져오기 ( 최소 주문 가격 포함 )
-
+    
+    // 상점 정보 조회
     const storeResult = await db.collection('store').findOne({ _id: new ObjectId(storeId) });
-    const minDeliveryPrice = storeResult.min_delivery_price;
-    const menuResult = await db.collection('menu').findOne({_id : new ObjectId(storeResult.menuId)})
+    const { min_delivery_price, title, delivery_time, delivery_tip, menuId: storeMenuId } = storeResult;
+    
+    // 메뉴 정보 조회
+    const menuResult = await db.collection('menu').findOne({_id : new ObjectId(storeMenuId)});
    
     // menuInfoArray에서 menuId에 해당하는 항목 찾기
     const menuInfoArray = menuResult.menu_info;
     const selectedMenuItem = menuInfoArray.find(item => item.id === menuId);
-    res.status(200).json({ minDeliveryPrice, selectedMenuItem });
+    
+    res.status(200).json({
+      minDeliveryPrice: min_delivery_price,
+      title,
+      delivery_time,
+      delivery_tip,
+      selectedMenuItem
+    });
   } catch (error) {
     console.error("API 요청 에러:", error);
     res.status(500).json({ error: "API 요청 에러" });
