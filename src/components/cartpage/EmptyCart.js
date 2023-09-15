84b5f6ed9d510065af0d0b581fe797f896c54507
Mintaek2 <789rkd@naver.com>
2023-09-15 15:17:31 +0900

    refact : image quality

--- src/components/cartpage/EmptyCart.js
+++ src/components/cartpage/EmptyCart.js
@@ -1,19 +1,24 @@
-import * as S from "./CartStyle"
-import empty from "/public/assets/img/empty.png"
-import plus from "/public/assets/img/plus.png"
-import Image from "next/image"
-import Link from "next/link"
-export default function EmptyCart(){
-    return(
-        <S.EmptyContainer>
-            <Image src={empty} alt="empty-icon" style={{marginBottom:"35px"}}/>
-            <S.EmptyText>장바구니가 텅 비었어요</S.EmptyText>
-            <Link href='/delivery'>
-            <S.EmptyBtn>
-                <Image src={plus} alt="plus-icon" width={18}/>
-                <span>더 담으러 가기</span>
-            </S.EmptyBtn>
-            </Link>
-        </S.EmptyContainer>
-    )
+import * as S from "./CartStyle";
+import empty from "/public/assets/img/empty.png";
+import plus from "/public/assets/img/plus.png";
+import Image from "next/image";
+import Link from "next/link";
+export default function EmptyCart() {
+  return (
+    <S.EmptyContainer>
+      <Image
+        src={empty}
+        alt="empty-icon"
+        style={{ marginBottom: "35px" }}
+        quality={75}
+      />
+      <S.EmptyText>장바구니가 텅 비었어요</S.EmptyText>
+      <Link href="/delivery">
+        <S.EmptyBtn>
+          <Image src={plus} alt="plus-icon" width={18} />
+          <span>더 담으러 가기</span>
+        </S.EmptyBtn>
+      </Link>
+    </S.EmptyContainer>
+  );
 }
